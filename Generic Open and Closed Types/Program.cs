8b2a95ef258332dc1514280bb52e6b0c66380a9c
Kulpot <kulpotwahu@gmail.com>
2023-07-25 13:50:14 +0800

    Add project files.

---
+++ Generic Open and Closed Types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=VXCUp-qE9bU&list=PLRwVmtr-pp05ETzLYQ8kv1cI5v9ddT8er&index=7
+//
+
+class MyClass<Tahahah>    // open type
+{
+    public static int Value;
+    static MyClass()
+    {
+        Console.WriteLine(typeof(MyClass<Tahahah>));
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        MyClass<string>.Value = 10; // closed type
+        Console.WriteLine(typeof(MyClass<>));
+        //MyClass<MyClass<string>>.Value = 10;    // two closed type
+        //MyClass<string>.Value = 20; // closed type
+    }
+}
